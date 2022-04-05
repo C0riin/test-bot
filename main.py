f05070645070a97a4defdcd5206332e2c8eff811
C0riin <102750574+C0riin@users.noreply.github.com>
2022-04-05 18:58:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://<Название_вашего_хероку_приложения>.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://dashboard.heroku.com/apps/test0000101/deploy/heroku-git' + TOKEN)
     return "!", 200
 
 if __name__ == "__main__":
